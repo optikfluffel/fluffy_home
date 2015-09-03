e6c4fc0ffd292abdee4333fbc7fecd278d59ba9d
optikfluffel <optik@fluffel.io>
2015-09-03 11:13:00 +0200

    add raspi config for "production" use

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :fluffy_home, FluffyHome.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "raspberrypi", port: 8080],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
